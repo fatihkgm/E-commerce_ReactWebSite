3ae5ee044666ef99d62eb296580a3e422cedd4ee
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-09 10:21:48 -0500

    ShopPage array implemented

--- shopyweb/src/pages/shopy/shopy.component.jsx
+++ shopyweb/src/pages/shopy/shopy.component.jsx
@@ -1,14 +1,22 @@
-import React from "react";
-import SHOP_DATA from "./shopydata.js";
+import React from 'react';
+import SHOP_DATA from './shopydata.js';
+import CollectionView from '../../components/collectionView/collection_view';
 
 class ShopyPage extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { collection: SHOP_DATA };
+    this.state = { collections: SHOP_DATA };
   }
   render() {
-    return <div>SHOPY PAGE</div>;
+    const { collections } = this.state;
+    return (
+      <div className="shop_page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionView key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
