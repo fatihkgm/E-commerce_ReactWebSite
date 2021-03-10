23edc814f53bf0c11ba8447c9b16bd459529a9f3
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-10 18:49:31 -0500

    Home link implemented

--- shopyweb/src/components/header/header.jsx
+++ shopyweb/src/components/header/header.jsx
@@ -10,6 +10,9 @@ const Header = ({ currentUser }) => (
       <Logo className="logo" />
     </Link>
     <div className="options">
+      <Link className="option" to="/">
+        HOME
+      </Link>
       <Link className="option" to="/shopy">
         SHOPY
       </Link>
