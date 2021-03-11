34eec8c1993db95931610dc81fceba3a55978df8
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-11 17:08:49 -0500

    cart-item array implemented

--- shopyweb/src/components/cart/cart_dropdown.jsx
+++ shopyweb/src/components/cart/cart_dropdown.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CustomButton from '../custom_button/button';
+import CartItem from '../cart_item/cart_item';
 import './cart_dropdown.scss';
 
-const CartDrop = () => (
+const CartDrop = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-item" />
+    <div className="cart-items">
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDrop;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(CartDrop);
