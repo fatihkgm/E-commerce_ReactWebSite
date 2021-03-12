e6eaa064d371bd55ebb1c0b5ab7f5f9f5d77ea29
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-12 00:23:49 -0500

    CheckOut component updated and style implemented

--- shopyweb/src/components/cart_icon/cart_icon.jsx
+++ shopyweb/src/components/cart_icon/cart_icon.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
-import { seletCartItemsCount } from '../../redux/cart/cart.selector';
+import { selectCartItemsCount } from '../../redux/cart/cart.selector';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { createStructuredSelector } from 'reselect';
 import './cart_icon.scss';
@@ -17,6 +17,6 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const mapStateToProps = createStructuredSelector({
-  itemCount: seletCartItemsCount,
+  itemCount: selectCartItemsCount,
 });
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
--- shopyweb/src/pages/checkout/checkout.jsx
+++ shopyweb/src/pages/checkout/checkout.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import CheckoutItem from '../../components/checkout_item/checkout_item';
+import {
+  selectCartItems,
+  selectCartTotal,
+} from '../../redux/cart/cart.selector';
 import './checkout.style.scss';
 
-const CheckOutPage = () => {
-  <div>CHECKOUT PAGE</div>;
-};
+const CheckoutPage = ({ cartItems, total }) => (
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div className="header-block">
+        <span>Product</span>
+      </div>
+      <div className="header-block">
+        <span>Description</span>
+      </div>
+      <div className="header-block">
+        <span>Quantity</span>
+      </div>
+      <div className="header-block">
+        <span>Price</span>
+      </div>
+      <div className="header-block">
+        <span>Remove</span>
+      </div>
+    </div>
+    {cartItems.map((cartItem) => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
+    <div className="total">TOTAL: ${total}</div>
+  </div>
+);
 
-export default CheckOutPage;
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal,
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
